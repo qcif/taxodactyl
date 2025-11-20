ff641794ebaa4dcc2ccf693d709821a1cbebcbfd
Cameron Hyde <chyde@neoformit.com>
2025-11-20 14:09:56 +1000

    Fix unit tests

--- scripts/tests/test_phylogeny_sampling.py
+++ scripts/tests/test_phylogeny_sampling.py
@@ -5,9 +5,24 @@ from unittest import mock
 
 from scripts import p3_assign_taxonomy
 
+TEST_DATA_DIR = Path(__file__).parent / "test-data"
+
+# -----------------------------------------------------------------------------
+# Test data with 32 hits across 4 species (8 each), identities 0.921-0.999
+# Strong candidates:
+# - Homo (0.980-0.999)
+# Moderate candidates:
+# - Gorilla (0.940-0.949),
+# - Pongo (0.960-0.969)
+# Non-candidates:
+# - Pan (0.921-0.930)
+
+TEST_HITS_JSON = TEST_DATA_DIR / "test_phylogeny_hits.json"
+# -----------------------------------------------------------------------------
+
 
 class TestGetAccessionsForPhylogeny(unittest.TestCase):
-    """Unit-tests for sampling of hits for phylogenetic subject sequences."""
+    """Test sampling of hit sequences for building genetic distance tree."""
 
     CRITERIA_CONFIG = {
         'phylogeny_min_hit_identity': 0.935,
@@ -28,57 +43,14 @@ class TestGetAccessionsForPhylogeny(unittest.TestCase):
             patcher.start()
             self.addCleanup(patcher.stop)
 
-        # Load 32 hits across 4 species (8 each), identities 0.921-0.999
-        # ~ Candidates:
-        # Homo (0.980-0.999)
-        # ~ Non-candidates:
-        # Pan (0.921-0.930)
-        # Gorilla (0.940-0.949),
-        # Pongo (0.960-0.969)
-
-        test_data_path = (
-            Path(__file__).parent / "test-data" / "test_phylogeny_hits.json"
-        )
-        with open(test_data_path) as f:
-            hits = json.load(f)
-        self.result = p3_assign_taxonomy._get_accessions_for_phylogeny(
-            hits, id_key="acc", identity_key="identity"
-        )
+        with open(TEST_HITS_JSON) as f:
+            self.hits = json.load(f)
 
     def test_accession_selection_with_strict_candidate(self):
-        results_enumerated = {
-            key: len([
-                x for x in self.result
-                if x[:2] == key
-            ])
-            for key in (
-                'HS',
-                'GG',
-                'PP',
-                'PT',
-            )
-        }
-        self.assertIn('HS1', self.result)
-        self.assertIn('HS8', self.result)
-        self.assertIn('GG1', self.result)
-        self.assertIn('GG8', self.result)
-        self.assertIn('PP1', self.result)
-        self.assertIn('PP8', self.result)
-        self.assertIn('PT1', self.result)
-        self.assertIn('PT5', self.result)
-        self.assertIn('PT8', self.result)
-        self.assertEqual(results_enumerated['HS'], 5)
-        self.assertEqual(results_enumerated['GG'], 3)
-        self.assertEqual(results_enumerated['PP'], 3)
-        self.assertEqual(results_enumerated['PT'], 3)
-
-    def test_accession_selection_with_moderate_candidate(self):
-        """Remove strong candidate to make all species candidates."""
-        result = [
-            x for x in self.results.copy()
-            if not x.startswith('HS')
-        ]
-        results_enumerated = {
+        result = p3_assign_taxonomy._get_accessions_for_phylogeny(
+            self.hits, id_key="acc", identity_key="identity"
+        )
+        result_enumerated = {
             key: len([
                 x for x in result
                 if x[:2] == key
@@ -99,9 +71,40 @@ class TestGetAccessionsForPhylogeny(unittest.TestCase):
         self.assertIn('PT1', result)
         self.assertIn('PT5', result)
         self.assertIn('PT8', result)
+        self.assertEqual(result_enumerated['HS'], 5)
+        self.assertEqual(result_enumerated['GG'], 3)
+        self.assertEqual(result_enumerated['PP'], 3)
+        self.assertEqual(result_enumerated['PT'], 3)
+
+    def test_accession_selection_with_moderate_candidate(self):
+        """Remove strong candidate to make 2 species moderate candidates."""
+        hits = [
+            x for x in self.hits.copy()
+            if not x['acc'].startswith('HS')
+        ]
+        result = p3_assign_taxonomy._get_accessions_for_phylogeny(
+            hits, id_key="acc", identity_key="identity"
+        )
+        results_enumerated = {
+            key: len([
+                x for x in result
+                if x[:2] == key
+            ])
+            for key in (
+                'GG',
+                'PP',
+                'PT',
+            )
+        }
+        self.assertIn('GG1', result)
+        self.assertIn('GG8', result)
+        self.assertIn('PP1', result)
+        self.assertIn('PP8', result)
+        self.assertIn('PT1', result)
+        self.assertIn('PT8', result)
         self.assertEqual(results_enumerated['GG'], 5)
         self.assertEqual(results_enumerated['PP'], 5)
-        self.assertEqual(results_enumerated['PT'], 5)
+        self.assertEqual(results_enumerated['PT'], 3)
 
     def test_sampling_when_above_limit(self):
         # 13 hits for one species → sampling branch
