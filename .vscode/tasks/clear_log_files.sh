9239a757ab076ace4713b8946a0f0cf6382603a9
Cameron Hyde <chyde@neoformit.com>
2026-04-30 10:56:08 +1000

    Fix log files cleanup vscode task

--- .vscode/tasks/clear_log_files.sh
+++ .vscode/tasks/clear_log_files.sh
@@ -1,4 +1,4 @@
 # Delete log files/directories in the output directory
 
 find scripts/output -type f -name '*.log' -delete
-find scripts/output -type d -name 'errors' -exec rm -rf {} \; || true
+rm -rf scripts/output/*/errors/
